3d2e4752f82d8baed904ce24eff144bf5299247e
DESKTOP-E3NKAR6\Jeevitra Sree <jeevitrasreeram@gmail.com>
2026-04-02 17:43:58 +0800

    chore: initialize project with package.json and express dependency

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import products from './data/products.js';
+
+const port = 5000;
+
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+app.get('/api/products', (req, res) => {
+  res.json(products);
+});
+
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).json({ message: 'Product not found' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
